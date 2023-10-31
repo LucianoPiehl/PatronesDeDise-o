e25fdbb8df1c0b13b65d442beb84c1ae94ee692b
Luciano Piehl <110488084+LucianoPiehl@users.noreply.github.com>
2023-10-31 12:54:46 -0300

    Delete Facade Patron directory

--- Facade Patron/src/FacturacionSubsystem.java
+++
@@ -1,7 +0,0 @@
-public class FacturacionSubsystem {
-    private int nFractura;
-    public int generarFactura(int clienteId, int productoId, int cantidad){
-        System.out.println("Generando factura");
-        return nFractura;
-    }
-}
--- Facade Patron/src/InventarioSubsystem.java
+++
@@ -1,7 +0,0 @@
-public class InventarioSubsystem {
-    public boolean verificarDisponibilidadProducto(int productoId, int cantidad) {
-        System.out.println("Verificando disponibilidad");
-        System.out.println("Producto disponible");
-        return true;
-    }
-}
